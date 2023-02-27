52ed4157c8ef0b7a52602db533e6f61652c15708
mcsrk <49255542+mcsrk@users.noreply.github.com>
2023-02-27 00:31:54 -0500

    added email verification before send

--- app/utils/email.utils.js
+++ app/utils/email.utils.js
@@ -1,8 +1,18 @@
 const nodemailer = require("nodemailer");
+const AWS = require("aws-sdk");
 
-const buildEmailOptions = ({ emailTo, subject, text, html, fileName }) => {
+// Configure AWS SES Credentials
+AWS.config.update({
+	region: "us-east-1",
+	accessKeyId: "AKIARKCUO56XS2KNOQKF",
+	secretAccessKey: "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",
+});
+
+const SES = new AWS.SES({ apiVersion: "2010-12-01" });
+
+const buildEmailOptions = ({ emailFrom, emailTo, subject, text, html, fileName }) => {
 	return {
-		from: "foo@example.com", // sender address
+		from: emailFrom, // sender address
 		to: emailTo, // list of receivers
 		subject: subject, // Subject line
 		text, // plain text body
@@ -17,18 +27,40 @@ const buildEmailOptions = ({ emailTo, subject, text, html, fileName }) => {
 	};
 };
 
-const sendEmail = async (mailOptions) => {
-	// create reusable transporter object using the default SMTP transport
-	let transporter = nodemailer.createTransport({
-		host: "sandbox.smtp.mailtrap.io",
-		port: 2525,
-		auth: {
-			user: "7594c769fd6c27", // Fake credential from Mailtrap
-			pass: "f9cc630b65b57d",
-		},
-	});
+const verifyEmailInAWS = async (email) => {
+	try {
+		const params = {
+			Identities: [email],
+		};
 
-	// send mail with defined transport object
+		const data = await SES.getIdentityVerificationAttributes(params).promise();
+		const identityInfo = data.VerificationAttributes[email];
+
+		// If already verified
+		if (identityInfo && identityInfo.VerificationStatus === "Success") {
+			console.log("La dirección de correo electrónico ya está verificada");
+			return { status: "success", message: "La dirección de correo electrónico ya está verificada" };
+		} else {
+			console.log("La dirección de correo electrónico no está verificada");
+			const verifyParams = {
+				EmailAddress: email,
+			};
+
+			const result = await SES.verifyEmailIdentity(verifyParams).promise();
+			console.log("Solicitud de verificación enviada: ", result);
+			return { status: "success", message: "Solicitud de verificación enviada al correo:" + email };
+		}
+	} catch (error) {
+		console.error(error);
+		return { status: "error", message: error.message };
+	}
+};
+
+const sendEmail = async (mailOptions) => {
+	// Create reusable transporter object using the default SMTP transport
+	const transporter = nodemailer.createTransport({
+		SES,
+	});
 
 	let info = await transporter.sendMail(mailOptions);
 	console.log("Email sent successfully:", info.response);
@@ -37,6 +69,7 @@ const sendEmail = async (mailOptions) => {
 };
 
 module.exports = {
+	verifyEmailInAWS,
 	sendEmail,
 	buildEmailOptions,
 };
